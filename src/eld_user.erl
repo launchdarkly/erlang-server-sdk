827c1c43cfe9d3071c10a48e8046997f17fa0fdd
unix1 <zurab@launchdarkly.com>
2019-06-26 16:57:48 +0000

    Add user setter, new user creation from map (#19)

--- src/eld_user.erl
+++ src/eld_user.erl
@@ -8,7 +8,9 @@
 
 %% API
 -export([new/1]).
+-export([new_from_map/1]).
 -export([get/2]).
+-export([set/3]).
 -export([scrub/1]).
 
 %% Types
@@ -44,6 +46,10 @@
 new(Key) when is_binary(Key) ->
     #{key => Key}.
 
+-spec new_from_map(map()) -> user().
+new_from_map(Map) ->
+    maps:fold(fun set/3, #{}, Map).
+
 %% @doc Get an attribute value of a user
 %%
 %% Lookup includes custom attributes. Returns `null' if attribute doesn't exist.
@@ -52,6 +58,16 @@ new(Key) when is_binary(Key) ->
 get(Attribute, User) ->
     Attr = get_attribute(Attribute),
     get_attribute_value(Attr, User).
+
+%% @doc Set an attribute value for a user
+%%
+%% Sets given attribute to given value and returns the new user. This function
+%% can handle both built-in and custom user attributes.
+%% @end
+-spec set(attribute(), any(), user()) -> user().
+set(Attribute, Value, User) ->
+    Attr = get_attribute(Attribute),
+    set_attribute_value(Attr, Value, User).
 
 %% @doc Scrub private attributes from user
 %%
@@ -78,6 +94,17 @@ get_attribute_value(Attr, _) when is_binary(Attr) ->
     null.
 
 -spec get_attribute(attribute()) -> attribute().
+get_attribute(key) -> key;
+get_attribute(secondary) -> secondary;
+get_attribute(ip) -> ip;
+get_attribute(country) -> country;
+get_attribute(email) -> email;
+get_attribute(first_name) -> first_name;
+get_attribute(last_name) -> last_name;
+get_attribute(avatar) -> avatar;
+get_attribute(name) -> name;
+get_attribute(anonymous) -> anonymous;
+get_attribute(Attribute) when is_atom(Attribute) -> atom_to_binary(Attribute, utf8);
 get_attribute(<<"key">>) -> key;
 get_attribute(<<"secondary">>) -> secondary;
 get_attribute(<<"ip">>) -> ip;
@@ -87,8 +114,15 @@ get_attribute(<<"first_name">>) -> first_name;
 get_attribute(<<"last_name">>) -> last_name;
 get_attribute(<<"avatar">>) -> avatar;
 get_attribute(<<"name">>) -> name;
-get_attribute(Attribute) when is_binary(Attribute) -> Attribute;
-get_attribute(Attribute) when is_atom(Attribute) -> Attribute.
+get_attribute(<<"anonymous">>) -> anonymous;
+get_attribute(Attribute) when is_binary(Attribute) -> Attribute.
+
+-spec set_attribute_value(Attr :: attribute(), Value :: any(), User :: user()) -> user().
+set_attribute_value(Attr, Value, User) when is_atom(Attr) ->
+    maps:put(Attr, Value, User);
+set_attribute_value(Attr, Value, User) when is_binary(Attr) ->
+    Custom = maps:get(custom, User, #{}),
+    maps:put(custom, Custom#{Attr => Value}, User).
 
 -spec scrub_private_attributes(user(), private_attribute_names()) -> user().
 scrub_private_attributes(User, []) ->
