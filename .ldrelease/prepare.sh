eedcc16e8c971c5e7e04634600a5923055dadc20
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2021-09-30 08:50:44 -0700

    ch64830 Updates to improve the release process for erlang. (#93)
    * ch64830 Initial pass at changes to the release process.
    
    * ch64830 Add secrets configuration and update scripts.
    
    * ch64830 Change prepare script to be more concise.
    
    * ch64830 Unspecified image version.
    
    * ch64830 Update prepare script. Add version back to docker image.
    
    * ch64830 Update releaser config version.
    
    * ch64830 Change capitalization gitHubPages.
    
    * ch64830 Add zip.
    
    * ch64830 Put the docs where releaser expects them.
    
    * ch64830 Test publishing.
    
    * ch64830 Test publishing.
    
    * ch64830 Create directory for hex config.
    
    * ch64830 Fix formatting of hex.config.
    
    * ch64830 Add hex plugin to rebar.
    
    * ch64830 Change executable permissions.
    
    * ch64830 Remove testing code.
    
    * ch64830 Add newline to end of file.

---
+++ .ldrelease/prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Make the directory the configuration should be stored in.
+mkdir -p ~/.config/rebar3/
+
+# Create rebar config with hex plugin.
+  cat >~/.config/rebar3/rebar.config <<EOF
+{plugins, [rebar3_hex]}.
+EOF
+
+if [ -z "${LD_RELEASE_DRY_RUN:-}" ]; then
+  hex_api_key="$(cat "${LD_RELEASE_SECRETS_DIR}/hex_api_key")"
+
+  # Create a hex.config file to allow publishing.
+  # Note: The indentation here translates to the output file,
+  # and the EOF must be at the start of the line.
+  cat >~/.config/rebar3/hex.config <<EOF
+%% coding: utf-8
+#{<<"hexpm">> =>
+  #{username => <<"launchdarkly">>,
+    api_key => <<"${hex_api_key}">>}}.
+EOF
+
+fi
