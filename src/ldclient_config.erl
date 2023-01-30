60947283f7b1853cbd5ef25be56536d772320c74
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2023-01-30 23:00:45 +0000

    Releasing version 1.6.0

--- src/ldclient_config.erl
+++ src/ldclient_config.erl
@@ -107,7 +107,7 @@
 -define(DEFAULT_POLLING_UPDATE_REQUESTOR, ldclient_update_requestor_httpc).
 -define(MINIMUM_POLLING_INTERVAL, 30).
 -define(USER_AGENT, "ErlangClient").
--define(VERSION, "1.5.0").
+-define(VERSION, "1.6.0").
 -define(EVENT_SCHEMA, "3").
 -define(DEFAULT_OFFLINE, false).
 -define(DEFAULT_REDIS_HOST, "127.0.0.1").
