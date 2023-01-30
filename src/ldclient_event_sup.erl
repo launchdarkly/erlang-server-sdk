34ddac70b2f99fda1ea161f1ffe3c749583328d1
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-01-30 10:41:55 -0800

    build(deps): Bump lru version to 2.4.0 (#132)

--- src/ldclient_event_sup.erl
+++ src/ldclient_event_sup.erl
@@ -37,7 +37,7 @@ init([Tag]) ->
 children(Tag) ->
     UserCacheName = ldclient_user_cache:get_local_reg_name(Tag),
     UserKeysCapacity = ldclient_config:get_value(Tag, user_keys_capacity),
-    UserCacheWorker = ?CHILD(lru, lru, [{local, UserCacheName}, UserKeysCapacity, []], worker),
+    UserCacheWorker = ?CHILD(lru, lru, [{local, UserCacheName}, [{max_objs, UserKeysCapacity}]], worker),
     EventStorageWorker = ?CHILD(ldclient_event_server, ldclient_event_server, [Tag], worker),
     EventProcessWorker = ?CHILD(ldclient_event_process_server, ldclient_event_process_server, [Tag], worker),
     [UserCacheWorker, EventStorageWorker, EventProcessWorker].
